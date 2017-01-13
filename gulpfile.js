251156e7add6c02e614f76f630001c4ac47e7833
jpass <suchomski1@gmail.com>
2017-01-13 10:19:02 +0100

    moved main css inside theme

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+gulp.task('sass', function() {
+    return sass('Resources/public/assets/sass/**/*.sass')
+        .pipe(gulp.dest('Resources/public/assets/css/dist'))
+});
+
+gulp.task('watch', function(){
+    return gulp.watch('Resources/public/assets/sass/**/*.sass', function(){
+        gulp.run('sass');
+    });
+});
+
+gulp.task('default', function() {
+    gulp.run('sass');
+});
