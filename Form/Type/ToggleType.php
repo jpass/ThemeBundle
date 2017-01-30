239824e02d9d2c18cec47f22288ff622787966e0
jpass <suchomski1@gmail.com>
2017-01-30 09:54:38 +0100

    added view for toggle form type

--- Form/Type/ToggleType.php
+++ Form/Type/ToggleType.php
@@ -3,11 +3,21 @@
 namespace Jpass\ThemeBundle\Form\Type;
 
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ToggleType
+class ToggleType extends AbstractType
 {
-    public function configureOptions(OptionsResolver $resolver)
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
+        $view->vars = array_replace($view->vars, array(
+            'toggle' => true
+        ));
     }
 
     public function getParent()
